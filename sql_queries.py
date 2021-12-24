05d103c86f76300ba6e007415b73f035a7458469
Raphael Fritz <raphael.fritz@posteo.de>
2021-12-24 08:33:48 +0100

    Add 'CASCADE' to DROP TABLE statements.

--- sql_queries.py
+++ sql_queries.py
@@ -21,10 +21,10 @@ SELECT DISTINCT tablename
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
+user_table_drop = "DROP TABLE IF EXISTS users CASCADE"
+song_table_drop = "DROP TABLE IF EXISTS songs CASCADE"
+artist_table_drop = "DROP TABLE IF EXISTS artists CASCADE"
+time_table_drop = "DROP TABLE IF EXISTS time CASCADE"
 
 # TRUNCATE TABLES
 
